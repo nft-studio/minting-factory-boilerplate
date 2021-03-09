0279380554b7c9d2772a368367b2fcfae29e1457
turinglabsorg <s.cataudo@turinglabs.org>
2021-03-09 08:09:00 +0100

    adding buefy and hash calculation

--- src/components/Mint.vue
+++ src/components/Mint.vue
@@ -2,14 +2,25 @@
   <div>
     Minting from {{ account }}
     <hr />
-    <input v-model="toMint" />
-    <button v-on:click="mint">MINT!</button>
+    <div v-if="toMint">You're minting {{ toMint }}</div>
+    <b-field>
+      <b-upload v-model="dropFile" v-if="!toMint" drag-drop>
+        <section class="section">
+          <div class="content has-text-centered">
+            <p>Drop your file here or click to upload</p>
+          </div>
+        </section>
+      </b-upload>
+    </b-field>
+    <button v-if="toMint" v-on:click="mint">MINT!</button>
   </div>
 </template>
 
 <script>
 var Web3 = require("web3");
 const ABI = require("../util/abi.json");
+const crypto = require("crypto");
+
 export default {
   name: "Mint",
   data() {
@@ -18,6 +29,7 @@ export default {
       contractAddress: "0xfF2868445fb4360704173B1c5b7ae785eF5dA47B",
       account: "",
       contract: {},
+      dropFile: {},
       toMint: "",
     };
   },
@@ -38,7 +50,24 @@ export default {
         .mintLoop(this.toMint)
         .send({ from: this.account });
       alert("Successfully minted at: " + minted.transactionHash);
-      console.log(minted)
+      console.log(minted);
+      this.toMint = "";
+      this.dropFile = {};
+    },
+  },
+  watch: {
+    dropFile() {
+      const app = this;
+      var reader = new FileReader();
+      reader.onload = function (event) {
+        var readed = event.target.result;
+        let hash = crypto
+          .createHash("sha256")
+          .update(new Uint8Array(readed))
+          .digest("hex");
+        app.toMint = "0x" + hash;
+      };
+      reader.readAsArrayBuffer(app.dropFile);
     },
   },
 };
