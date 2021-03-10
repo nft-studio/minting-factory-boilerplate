cbc8a7e1358ca68e34bc2fc95b8fdf357c926936
turinglabsorg <s.cataudo@turinglabs.org>
2021-03-10 08:20:55 +0100

    min interface fixes

--- src/components/Mint.vue
+++ src/components/Mint.vue
@@ -1,50 +1,67 @@
 <template>
   <div>
-    Minting from {{ account }}
-    <hr />
-    <div v-if="toMint">You're minting {{ toMint }}</div>
-    <b-field>
-      <b-upload v-model="dropFile" v-if="!toMint" drag-drop>
-        <section class="section">
-          <div class="content has-text-centered">
-            <p>Drop your file here or click to upload</p>
-          </div>
-        </section>
-      </b-upload>
-    </b-field>
-    <button v-if="toMint" v-on:click="mint">MINT!</button>
+    <div v-if="account">
+      <b>Welcome back</b><br><i style="font-size:12px">{{ account }}</i>
+      <hr />
+      <div style="padding: 0 20px">
+        <b-field label="IPFS Hash">
+          <b-input v-model="toMint"></b-input>
+        </b-field>
+        <b-field label="Name">
+          <b-input v-model="name"></b-input>
+        </b-field>
+        <b-field label="Description">
+          <b-input
+            maxlength="200"
+            v-model="description"
+            type="textarea"
+          ></b-input>
+        </b-field>
+        <b-button type="is-primary" expanded v-on:click="mint">MINT!</b-button>
+      </div>
+    </div>
+    <div v-if="!account">
+      Please connect your Metamask wallet first,<br>window should be open
+      automatically or click below button.<br /><br>
+      <b-button type="is-primary" v-on:click="connect"
+        >CONNECT METAMASK</b-button
+      >
+    </div>
   </div>
 </template>
 
 <script>
 var Web3 = require("web3");
 const ABI = require("../util/abi.json");
-const crypto = require("crypto");
+const axios = require("axios");
 
 export default {
   name: "Mint",
   data() {
     return {
-      web3: new Web3("ws://localhost:7545"),
-      contractAddress: "0xfF2868445fb4360704173B1c5b7ae785eF5dA47B",
+      web3: new Web3(process.env.VUE_APP_NETWORK),
+      contractAddress: process.env.VUE_APP_SMART_CONTRACT_ADDRESS,
       account: "",
       contract: {},
-      dropFile: {},
+      name: "",
+      description: "",
       toMint: "",
+      axios: axios,
     };
   },
-  async mounted() {
-    window.ethereum.enable();
-    let accounts = await this.web3.eth.getAccounts();
-    let contract = await new this.web3.eth.Contract(ABI, this.contractAddress, {
-      gasLimit: "5000000",
-    });
-    this.contract = contract;
-    this.account = accounts[0];
-    console.log(accounts);
-    console.log(contract);
+  mounted() {
+    this.connect()
   },
   methods: {
+    async connect(){
+      window.ethereum.enable();
+      let accounts = await this.web3.eth.getAccounts();
+      let contract = await new this.web3.eth.Contract(ABI, this.contractAddress, {
+        gasLimit: "5000000",
+      });
+      this.contract = contract;
+      this.account = accounts[0];
+    },
     async mint() {
       let minted = await this.contract.methods
         .mintLoop(this.toMint)
@@ -52,22 +69,6 @@ export default {
       alert("Successfully minted at: " + minted.transactionHash);
       console.log(minted);
       this.toMint = "";
-      this.dropFile = {};
-    },
-  },
-  watch: {
-    dropFile() {
-      const app = this;
-      var reader = new FileReader();
-      reader.onload = function (event) {
-        var readed = event.target.result;
-        let hash = crypto
-          .createHash("sha256")
-          .update(new Uint8Array(readed))
-          .digest("hex");
-        app.toMint = "0x" + hash;
-      };
-      reader.readAsArrayBuffer(app.dropFile);
     },
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img style="width:30%" src="../assets/logo.png">
+    <img style="width:30%" src="../assets/logo.jpg">
     <Mint />
   </div>
 </template>
